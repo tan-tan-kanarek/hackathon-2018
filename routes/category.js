e834ae70f995fe1cb33ebb805d0bb4b45a521ebc
tan-tan-kanarek <Jonathan.Kanarek@Kaltura.com>
2018-01-02 20:16:42 +0200

    a

--- routes/category.js
+++ routes/category.js
@@ -2,8 +2,10 @@
 
 const fs = require("fs");
 const md5 = require('md5');
+const url = require('url');
 const kaltura = require("kaltura-client");
 const express = require('express');
+const querystring = require('querystring');
 const router = express.Router();
 
 const {serviceUrl, partnerId, appToken} = JSON.parse(fs.readFileSync("config/kaltura.json"));
@@ -15,73 +17,115 @@ const client = new kaltura.Client(config);
 
 function startWidgetSession() {
     return new Promise((resolve, reject) => {
-            const widgetId = `_${partnerId}`;
-    kaltura.services.session.startWidgetSession(widgetId)
+        const widgetId = `_${partnerId}`;
+        kaltura.services.session.startWidgetSession(widgetId)
         .completion((success, response) => {
-        if(success) {
-            resolve(response.ks);
-        }
-        else {
-            reject(response);
-}
-})
-.execute(client);
-});
+            if(success) {
+                resolve(response.ks);
+            }
+            else {
+                reject(response);
+            }
+        })
+        .execute(client);
+    });
 }
 
 function startSession(widgetSession) {
     return new Promise((resolve, reject) => {
-            const tokenHash = md5(widgetSession + appToken.token);
-    kaltura.services.appToken.startSession(appToken.id, tokenHash)
+    	const tokenHash = md5(widgetSession + appToken.token);
+        kaltura.services.appToken.startSession(appToken.id, tokenHash)
         .setKs(widgetSession)
         .completion((success, response) => {
-        if(success) {
-            resolve(response.ks);
-        }
-        else {
-            reject(response);
-}
-})
-.execute(client);
-});
+            if(success) {
+                resolve(response.ks);
+            }
+            else {
+                reject(response);
+            }
+        })
+        .execute(client);
+    });
 }
 
-function listCategories(session) {
+function getMetadata(session, parentId) {
     return new Promise((resolve, reject) => {
 
-            var filter = new kaltura.objects.CategoryFilter({
-                fullNameStartsWith: "Market"
-            });
+        var itemsFilter = new kaltura.objects.CategoryFilter({
+        	parentIdEqual: parentId
+        });
 
-    kaltura.services.category.listAction(filter)
+        var liveStreamsFilter = new kaltura.objects.LiveStreamEntryFilter({
+        	categoriesIdsMatchAnd: parentId
+        });
+    
+    	var thumbnailsFilter = new kaltura.objects.MediaEntryFilter({
+    		tagsLike: "thumb",
+    		statusEqual: kaltura.enums.EntryStatus.NO_CONTENT
+    	});
+    
+    	var eventsFilter = new kaltura.objects.MediaEntryFilter({
+    	});
+    
+    	var entryFilterMapping = new kaltura.objects.ResponseProfileMapping({
+    		filterProperty: "categoriesIdsMatchAnd",
+    		parentProperty: "id"
+    	});
+    
+    	var thumbResponseProfile = new kaltura.objects.DetachedResponseProfile({
+    		name: "thumbs",
+    		filter: thumbnailsFilter,
+    		mappings: [entryFilterMapping]
+    	});
+    
+    	var eventsResponseProfile = new kaltura.objects.DetachedResponseProfile({
+    		name: "events",
+    		filter: eventsFilter,
+    		mappings: [entryFilterMapping]
+    	});
+    
+    	var responseProfile = new kaltura.objects.DetachedResponseProfile({
+    		relatedProfiles: [thumbResponseProfile, eventsResponseProfile]
+    	});
+
+        kaltura.services.category.listAction(itemsFilter)
+		.setResponseProfile(responseProfile)
+        .add(kaltura.services.liveStream.listAction(liveStreamsFilter))
         .setKs(session)
         .completion((success, response) => {
-        if(success) {
-            resolve({session: session, categories: response.objects});
-        }
-        else {
-            reject(response);
-}
-})
-.execute(client);
-});
+            if(success) {
+            	var items = response[0].objects;
+            	for(var i = 0; i < items.length; i++) {
+            		if(items[i].relatedObjects.events.totalCount) {
+            			for(var event in items[i].relatedObjects.events.objects) {
+            				
+            			}
+            		}
+            	}
+            	
+                resolve({
+                	session: session, 
+                	items: items, 
+                	liveStreams: response[1].objects
+                });
+            }
+            else {
+                reject(response);
+            }
+        })
+        .execute(client);
+    });
 }
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-    var url = require('url');
-    var querystring = require('querystring');
 
-
-    console.log("Orig URL = " + req.originalUrl);
-    var res = req.originalUrl.split("?");
-    console.log( "Query Params = " +  res[1]);
-    var qs = querystring.parse(res[1]);
-    console.log("ID = " +  qs.id);
+    var urlParts = req.originalUrl.split("?");
+    var query = querystring.parse(urlParts[1]);
+    
     startWidgetSession()
-        .then((widgetSession) => startSession(widgetSession))
-    .then((session) => listCategories(session))
-    .then(({session, categories}) => res.render('index', {ks: session, serviceUrl: serviceUrl, categories: categories}))
+    .then((widgetSession) => startSession(widgetSession))
+    .then((session) => getMetadata(session, query.id))
+    .then(({session, items, liveStreams}) => res.render('category', {ks: session, serviceUrl: serviceUrl, items: items, liveStreams: liveStreams}))
     .catch((err) => res.render('error', err));
 
 });
--- routes/index.js
+++ routes/index.js
@@ -15,71 +15,71 @@ const client = new kaltura.Client(config);
 
 function startWidgetSession() {
 	return new Promise((resolve, reject) => {
-			const widgetId = `_${partnerId}`;
-	kaltura.services.session.startWidgetSession(widgetId)
+		const widgetId = `_${partnerId}`;
+		kaltura.services.session.startWidgetSession(widgetId)
 		.completion((success, response) => {
-		if(success) {
-			resolve(response.ks);
-		}
-		else {
-			reject(response);
-}
-})
-.execute(client);
-});
+    		if(success) {
+    			resolve(response.ks);
+    		}
+    		else {
+    			reject(response);
+    		}
+    	})
+        .execute(client);
+    });
 }
 
 function startSession(widgetSession) {
 	return new Promise((resolve, reject) => {
-			const tokenHash = md5(widgetSession + appToken.token);
-	kaltura.services.appToken.startSession(appToken.id, tokenHash)
+		const tokenHash = md5(widgetSession + appToken.token);
+    	kaltura.services.appToken.startSession(appToken.id, tokenHash)
 		.setKs(widgetSession)
 		.completion((success, response) => {
-		if(success) {
-			resolve(response.ks);
-		}
-		else {
-			reject(response);
-}
-})
-.execute(client);
-});
+    		if(success) {
+    			resolve(response.ks);
+    		}
+    		else {
+    			reject(response);
+    		}
+        })
+        .execute(client);
+    });
 }
 
 function getMetadata(session) {
 	return new Promise((resolve, reject) => {
 
-			var categoriesFilter = new kaltura.objects.CategoryFilter({
-				fullNameStartsWith: "Market>",
-				tagsLike: "department"
-			});
+		var categoriesFilter = new kaltura.objects.CategoryFilter({
+			fullNameStartsWith: "Market>",
+			tagsLike: "department"
+		});
 
-	var itemsFilter = new kaltura.objects.CategoryFilter({
-		fullNameStartsWith: "Market>",
-		tagsLike: "item"
-	});
-
-	var thumbnailsFilter = new kaltura.objects.MediaEntryFilter({
-		tagsLike: "thumb",
-		statusEqual: kaltura.enums.EntryStatus.NO_CONTENT
-	});
-
-	var thumbnailsFilterMapping = new kaltura.objects.ResponseProfileMapping({
-		filterProperty: "categoriesIdsMatchAnd",
-		parentProperty: "id"
-	});
-
-	var thumbResponseProfile = new kaltura.objects.DetachedResponseProfile({
-		name: "thumbs",
-		filter: thumbnailsFilter,
-		mappings: [thumbnailsFilterMapping]
-	});
-
-	var responseProfile = new kaltura.objects.DetachedResponseProfile({
-		relatedProfiles: [thumbResponseProfile]
-	});
-	var rgbcolorsList = ["#00bfff","#0080ff","#0040ff","#DEB887","#CD853F", "#8B4513"];
-	kaltura.services.category.listAction(itemsFilter)
+    	var itemsFilter = new kaltura.objects.CategoryFilter({
+    		fullNameStartsWith: "Market>",
+    		tagsLike: "item"
+    	});
+    
+    	var thumbnailsFilter = new kaltura.objects.MediaEntryFilter({
+    		tagsLike: "thumb",
+    		statusEqual: kaltura.enums.EntryStatus.NO_CONTENT
+    	});
+    
+    	var thumbnailsFilterMapping = new kaltura.objects.ResponseProfileMapping({
+    		filterProperty: "categoriesIdsMatchAnd",
+    		parentProperty: "id"
+    	});
+    
+    	var thumbResponseProfile = new kaltura.objects.DetachedResponseProfile({
+    		name: "thumbs",
+    		filter: thumbnailsFilter,
+    		mappings: [thumbnailsFilterMapping]
+    	});
+    
+    	var responseProfile = new kaltura.objects.DetachedResponseProfile({
+    		relatedProfiles: [thumbResponseProfile]
+    	});
+    	var rgbcolorsList = ["#00bfff","#0080ff","#0040ff","#DEB887","#CD853F", "#8B4513"];
+    	kaltura.services.category.listAction(itemsFilter)
 		.setResponseProfile(responseProfile)
 		.add(kaltura.services.category.listAction(categoriesFilter))
 		.setKs(session)
@@ -100,17 +100,17 @@ function getMetadata(session) {
 		}
 		else {
 			reject(response);
-}
-})
-.execute(client);
-});
+		}
+    })
+    .execute(client);
+    });
 }
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
 
 	startWidgetSession()
-		.then((widgetSession) => startSession(widgetSession))
+	.then((widgetSession) => startSession(widgetSession))
 	.then((session) => getMetadata(session))
 	.then(({session, categories, items}) => res.render("index", {ks: session, serviceUrl: serviceUrl, categories: categories, items: items}))
 	.catch((err) => {
